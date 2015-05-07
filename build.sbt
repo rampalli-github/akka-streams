ede1e4fe0d081d0d09da77da3d0808fa15db39f3
rampalli-github <rampalli.github@gmail.com>
2015-05-07 22:48:17 +0530

    initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "hello"
+
+version := "1.0"
+
+scalaVersion := "2.11.6"
+
+libraryDependencies += "com.typesafe.akka" % "akka-stream-experimental_2.11" % "1.0-RC2"
---
+++ src/main/scala/sample/SampleStreams.scala
@@ -0,0 +1,41 @@
+package sample
+
+import akka.actor.ActorSystem
+import akka.stream.ActorFlowMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+
+object SampleStreams extends App{
+
+  implicit val system = ActorSystem()
+
+  implicit val materializer = ActorFlowMaterializer()
+
+  def streamIterator = Stream.from(1).iterator
+
+  iterateSrc(new IteratorWrapper(streamIterator))
+
+  def iterateSrc(iter: Iterator[Int]): Unit = {
+    val src = Source(() => iter)
+
+    val future = src.take(10).runWith(Sink.foreach(f => println("Int: "+f)))
+
+    future.onComplete {
+      case Success(x) => println("Complete")
+      case Failure(t) => println(t.getMessage)
+    }
+  }
+}
+
+class IteratorWrapper[T](iter: Iterator[T]) extends Iterator[T]{
+
+  val internalIterator = Stream.from(1).iterator
+
+  override def hasNext: Boolean = {
+    println("HasNext Called: "+internalIterator.next())
+    iter.hasNext
+  }
+
+  override def next(): T = iter.next()
+}
